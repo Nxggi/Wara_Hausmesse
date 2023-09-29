3facc8393e3919953eb2c6c1034000f7105c2118
Nxggi <146078210+Nxggi@users.noreply.github.com>
2023-09-29 10:50:52 +0200

    Add files via upload
    Update aller Dateien auf die Aktuellste version

--- Rudolf.py
+++ Rudolf.py
@@ -8,69 +8,75 @@ import threading
 import tkinter as tk
 import keyboard
 import time
-from tkinter import PhotoImage
-from PIL import Image, ImageTk
 
 running = True
 
+def start_gif_thread():
+    if __name__ == "__main__":
+        gif_thread = threading.Thread(target=gif_vid)
+        gif_thread.start()
+
+
+
+
+def update_gif():
+    try:
+        gif_image.seek(gif_image.tell() + 1)
+        frame = ImageTk.PhotoImage(gif_image)
+        gif_label.configure(image=frame)
+        gif_label.image = frame
+        fenster.after(100, update_gif)
+    except EOFError:
+        # Das GIF ist am Ende, starte es von vorne
+        gif_image.seek(0)
+        update_gif()
+
+
+def gif_vid():
+    global gif_image
+    global gif_label
+    gif_image = Image.open(r"C:\Users\1000len-6476\Desktop\Wara Hausmesse\Loading.gif")
+
+
+    frame = ImageTk.PhotoImage(gif_image)
+    gif_label = tk.Label(fenster, image=frame)
+    gif_label.pack()
+    fenster.after(100, update_gif)
+    fenster.mainloop()
+
+
+
 def Error_bei_Speech2text():
-        global running
-        Error = "Das habe ich leider nicht verstanden, bitte stelle deine Frage erneut"
-        fenster.after(0, lambda: label.config(text=Error))
-        Sprachausgabe(Error)
-        fenster.after(0, lambda: label.config(text="Drücke M zum aufnehmen deiner Frage"))
-        if running:
-            Hirn()
-        
-
-def Hirn():
     global running
-    while running:
-        WAV_Pfad =""
-        GPT_Antwort =""
-
-        keyboard.wait("m")
-        fenster.after(0, lambda: label.config(text="Recording..."))
-        WAV_Pfad = Eingabe()
-        fenster.after(0, lambda: label.config(text="Verarbeiten"))
-        Gesprochenes = S2T(WAV_Pfad)
-        if Gesprochenes == "Error§$§$§$§$§$§$$$§":
-            Error_bei_Speech2text()
-    #    print(Gesprochenes)
-    #    GPT_Antwort = ChatGPT(Gesprochenes) 
-        GPT_Antwort = ChatGPT_temp()
-        fenster.after(0, lambda: label.config(text=GPT_Antwort))
-        Sprachausgabe(GPT_Antwort)
-        time.sleep(1)
-        fenster.after(0, lambda: label.config(text="Drücke M zum aufnehmen deiner Frage"))
+    Error = "Das habe ich leider nicht verstanden, bitte stelle deine Frage erneut"
+    fenster.after(0, lambda: label.config(text=Error))
+    Sprachausgabe(Error)
+    fenster.after(0, lambda: label.config(text="Drücke M zum Aufnehmen deiner Frage"))
+    if running:
         Hirn()
 
-
 def GUI():
+    global fenster
+    global label
 
+    
     def close():
         global running
         running = False
         fenster.destroy()
         exit()
 
-
-
-    global fenster
+    
     fenster = tk.Tk()
     fenster.title("Rudolf")
-
-
+    fenster.configure(bg="white")
     w, h = fenster.winfo_screenwidth(), fenster.winfo_screenheight()
     fenster.overrideredirect(1)
     fenster.geometry("%dx%d+0+0" % (w, h))
-    
 
-    global label
-    label = tk.Label(fenster, text="Drücke M zum aufnehmen deiner Frage", font=("Arial", 24), anchor="center" )
-    #label.pack(padx=20, pady=20)
+    label = tk.Label(fenster, text="Drücke M zum Aufnehmen deiner Frage", font=("Arial", 24), anchor="center", bg="white")
     label.place(relx=0.5, rely=0.5, anchor="center")
-
+   
 
     button1 = tk.Button(fenster, text="Rudolf Beenden", command=close, width=20, height=3)
     button1.place(relx=0.5, rely=0.92, anchor="center")
@@ -78,16 +84,33 @@ def GUI():
     fenster.mainloop()
 
 
+def Hirn():
+    global gif_label  
+    global running
+  
+    while running:
+        WAV_Pfad = ""
+        GPT_Antwort = ""
+        keyboard.wait("m")
+        fenster.after(0, lambda: label.config(text="Recording..."))
+        WAV_Pfad = Eingabe()
+        start_gif_thread()
+        Gesprochenes = S2T(WAV_Pfad)
+        gif_label.pack_forget()
+        if Gesprochenes == "Error§$§$§$§$§$§$$$§":
+            Error_bei_Speech2text()
+        GPT_Antwort = ChatGPT_temp()
+        fenster.after(0, lambda: label.config(text=GPT_Antwort))
+        Sprachausgabe(GPT_Antwort)
+        time.sleep(1)
+        fenster.after(0, lambda: label.config(text="Drücke M zum Aufnehmen deiner Frage"))
 
 
 
 
 if __name__ == "__main__":
-    # Einen Thread für die GUI erstellen
     gui_thread = threading.Thread(target=GUI)
     gui_thread.start()
 
-    # Einen Thread für die Methode legLos erstellen
     leglos_thread = threading.Thread(target=Hirn)
     leglos_thread.start()
-
