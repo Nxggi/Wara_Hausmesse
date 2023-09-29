d34a06958e205dacd65773388e6915364178efa5
Nxggi <146078210+Nxggi@users.noreply.github.com>
2023-09-29 08:54:12 +0200

    Add files via upload
    neue versionen hochgeladen

--- Rudolf.py
+++ Rudolf.py
@@ -3,39 +3,55 @@ from Spracheingabe import Eingabe
 from ChatGPT import ChatGPT
 from ChatGPT import ChatGPT_temp
 from Sprachausgabe import Sprachausgabe
+from PIL import Image, ImageTk
 import threading
 import tkinter as tk
 import keyboard
+import time
+from tkinter import PhotoImage
+from PIL import Image, ImageTk
 
 
-
+def Error_bei_Speech2text():
+        Error = "Das habe ich leider nicht verstanden, bitte stelle deine Frage erneut"
+        fenster.after(0, lambda: label.config(text=Error))
+        Sprachausgabe(Error)
+        fenster.after(0, lambda: label.config(text="Drücke M zum aufnehmen deiner Frage"))
+        Hirn()
+        
 
 def Hirn():
     WAV_Pfad =""
     GPT_Antwort =""
+
     keyboard.wait("m")
     fenster.after(0, lambda: label.config(text="Recording..."))
     WAV_Pfad = Eingabe()
     fenster.after(0, lambda: label.config(text="Verarbeiten"))
     Gesprochenes = S2T(WAV_Pfad)
-    print(Gesprochenes)
-    #GPT_Antwort = ChatGPT(Gesprochenes) 
+    if Gesprochenes == "Error§$§$§$§$§$§$$$§":
+        Error_bei_Speech2text()
+#    print(Gesprochenes)
+#    GPT_Antwort = ChatGPT(Gesprochenes) 
     GPT_Antwort = ChatGPT_temp()
-    Sprachausgabe(GPT_Antwort)
     fenster.after(0, lambda: label.config(text=GPT_Antwort))
+    Sprachausgabe(GPT_Antwort)
+    time.sleep(1)
     fenster.after(0, lambda: label.config(text="Drücke M zum aufnehmen deiner Frage"))
     Hirn()
-
 
 
 def GUI():
 
     def close():
         fenster.destroy()
-        exit
+        exit()
+        
     global fenster
     fenster = tk.Tk()
     fenster.title("Rudolf")
+
+
     w, h = fenster.winfo_screenwidth(), fenster.winfo_screenheight()
     fenster.overrideredirect(1)
     fenster.geometry("%dx%d+0+0" % (w, h))
@@ -50,8 +66,10 @@ def GUI():
     button1 = tk.Button(fenster, text="Rudolf Beenden", command=close, width=20, height=3)
     button1.place(relx=0.5, rely=0.92, anchor="center")
 
-
     fenster.mainloop()
+
+
+
 
 
 
@@ -63,3 +81,4 @@ if __name__ == "__main__":
     # Einen Thread für die Methode legLos erstellen
     leglos_thread = threading.Thread(target=Hirn)
     leglos_thread.start()
+
--- Sprachausgabe.py
+++ Sprachausgabe.py
@@ -1,18 +1,41 @@
-import pyttsx3
+from gtts import gTTS
+import pygame
+import os
 
 
-def Sprachausgabe(GPT_AW):
-    #Die Antwort von GPT wird hier per Sprachausgabe und Textfeld ausgegeben
-    s = pyttsx3.init()    
-    s.setProperty('rate', 150)  
+def Sprachausgabe(text, language='de'):
+    # Erstellen Sie ein gTTS-Objekt
+    tts = gTTS(text=text, lang=language)
+    
+    # Holen Sie sich das aktuelle Verzeichnis des Skripts
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    
+    # Erstellen Sie den Dateipfad für die Ausgabedatei im aktuellen Verzeichnis
+    output_path = os.path.join(current_directory, "output.mp3")
+    
+    # Speichern Sie die Sprachausgabe als Ausgabedatei
+    tts.save(output_path)
 
-    voices = s.getProperty('voices')
-    german_voice_id = (s, "de_DE", "Till")
-    for voice in voices:
-        if "german" in voice.languages:
-            german_voice_id = voice.id
-            break
-    if german_voice_id:
-        s.setProperty('voice', german_voice_id)
-    s.say(GPT_AW)
-    s.runAndWait()
+    # Initialisieren Sie pygame Mixer
+    pygame.mixer.init()
+
+    # Laden Sie die Audiodatei als Sound-Objekt
+    sound = pygame.mixer.Sound(output_path)
+
+    # Spielen Sie die Audiodatei ab
+    sound.play()
+
+    # Warten, bis die Wiedergabe beendet ist
+    pygame.time.wait(int(sound.get_length() * 1000))  # Wartezeit in Millisekunden
+
+    # Löschen Sie die Ausgabedatei nach dem Abspielen
+    os.remove(output_path)
+
+
+    
+
+
+
+
+
+
